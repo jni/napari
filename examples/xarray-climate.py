ba78eb9cec2b90c85d77fbac46a329a5c23c3b4c
Juan Nunez-Iglesias <jni@fastmail.com>
2024-11-27 16:24:37 +1100

    Add xarray atmospheric data example

---
+++ examples/xarray-climate.py
@@ -0,0 +1,99 @@
+"""Viewing thredds wr45/ops_aps3 data in napari.
+
+Download bash commands:
+
+# download model prediction data
+curl -O https://thredds.nci.org.au/thredds/fileServer/wr45/ops_aps3/access-g/1/20241104/0000/fc/ml/air_temp.nc
+curl -O https://thredds.nci.org.au/thredds/fileServer/wr45/ops_aps3/access-g/1/20241104/0000/fc/ml/spec_hum.nc
+
+# download corresponding 10 days' worth of measurements
+mkdir an && cd an  # use 'an' folder for single time points
+for day in 04 05 06 07 08 09 10 11 12 13; do
+  for hour in 00 06 12 18; do
+    curl https://thredds.nci.org.au/thredds/fileServer/wr45/ops_aps3/access-g/1/202411${day}/${hour}00/an/ml/spec_hum.nc -o ${day}-${hour}-spec_hum.nc
+    curl https://thredds.nci.org.au/thredds/fileServer/wr45/ops_aps3/access-g/1/202411${day}/${hour}00/an/ml/air_temp.nc -o ${day}-${hour}-air_temp.nc
+  done
+done
+"""
+
+from glob import glob
+from pathlib import Path
+
+import numpy as np
+import xarray as xr
+
+import napari
+
+root_dir = Path('/Users/jni/data/thredds/20241104')
+
+def get_scale_translate(dataset, array_name, invert_lat=False):
+    """Get the translate/offset and scale parameters for an xarray dataset.
+
+    This code assumes that the dataset is regularly spaced. You should
+    interpolate your data if it is sampled at irregular spaces.
+
+    Parameters
+    ----------
+    dataset : xr.Dataset
+        The dataset containing the array to be displayed.
+    array_name : str
+        The name of the xarray DataArray within `dataset` to be displayed in
+        napari.
+    invert_lat : bool
+        Whether to invert the latitude values.
+
+        napari's axes follow zyx convention, with y pointing down.
+
+        latitude decreases going down conventionally (putting North at the top
+        and having southern latitudes be negative), so here we multiply by -1
+        to display the globe conventionally. Unfortunately, this means the
+        coordinates displayed by the viewer on hover will show northern
+        latitudes as negative. The true fix is to add a transformation
+        between the world/scene coordinates and the canvas in the napari code
+        base, but that might take some time.
+    """
+    array = getattr(dataset, array_name)
+    if array is None:
+        raise ValueError(f'{dataset} has no array with name {array_name}')
+    dims = [getattr(dataset, dim) for dim in array.dims]
+    translate = [float(d[0]) for d in dims]
+    scale = [float(d[1] - d[0]) for d in dims]
+    if invert_lat:
+        lat_pos, lat_name = next(
+                (i, dim)
+                for i, dim in enumerate(array.dims)
+                if dim.startswith('lat')
+                )
+        scale[lat_pos] *= -1
+        translate[lat_pos] = float(getattr(dataset, lat_name)[-1])
+    return {'scale': scale, 'translate': translate}
+
+
+an = xr.open_mfdataset(sorted(glob(str(root_dir / 'an/*spec_hum.nc'))))
+ds = xr.open_dataset(root_dir / 'spec_hum.nc', chunks={'time': 1})
+start, stop, step = [
+        np.array(elem)[()]
+        for elem in (ds.time[0], ds.time[-1], ds.time[1] - ds.time[0])
+        ]
+ds_reg = ds.interp(
+        coords={'time': np.arange(start, stop, step)},
+        method='nearest',
+        )
+
+viewer = napari.Viewer()
+model = viewer.add_image(
+        ds_reg.spec_hum,
+        name='model',
+        **get_scale_translate(ds_reg, 'spec_hum', invert_lat=True),
+        colormap='magenta',
+        )
+gt = viewer.add_image(
+        an.spec_hum,
+        name='measurement',
+        **get_scale_translate(an, 'spec_hum', invert_lat=True),
+        colormap='green',
+        blending='additive',
+        )
+
+if __name__ == '__main__':
+    napari.run()
